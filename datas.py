23bc2258da72fb885a280c65444d9235288bb525
waterflane <waterflane@gmail.com>
2026-04-16 14:48:41 +0300

    added data file

---
+++ datas.py
@@ -0,0 +1,6 @@
+# import kagglehub
+
+# https://www.kaggle.com/datasets/kkhandekar/all-datasets-for-practicing-ml
+
+# path = kagglehub.dataset_download("kkhandekar/all-datasets-for-practicing-ml")
+# print(path)
